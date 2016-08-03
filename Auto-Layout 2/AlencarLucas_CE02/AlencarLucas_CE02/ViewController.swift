36ef55296b15eaf74603dc601b1466ae096cfe11
lucasacw05 <lucasacw@gmail.com>
2016-08-03 13:24:05 -0400

    Second Lab Created

---
+++ Auto-Layout 2/AlencarLucas_CE02/AlencarLucas_CE02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlencarLucas_CE02
+//
+//  Created by Lucas Gabriel Cordeiro Alencar on 03/08/16.
+//  Copyright © 2016 Lucas Alencar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
