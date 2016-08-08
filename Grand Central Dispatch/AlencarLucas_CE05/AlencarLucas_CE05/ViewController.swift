182f36fbcfb65318c3ddff38c73232a25487b8c4
lucasacw05 <lucasacw@gmail.com>
2016-08-08 16:02:54 -0400

    Clear All Views function Done

--- Grand Central Dispatch/AlencarLucas_CE05/AlencarLucas_CE05/ViewController.swift
+++ Grand Central Dispatch/AlencarLucas_CE05/AlencarLucas_CE05/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    @IBOutlet weak var image4: UIImageView!
+    @IBOutlet weak var image5: UIImageView!
+    @IBOutlet weak var image6: UIImageView!
+    @IBOutlet weak var image7: UIImageView!
+    @IBOutlet weak var image8: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func clearAllViews(sender: AnyObject) {
+        
+        for image in [image1, image2, image3, image4, image5, image6, image7, image8] {
+            
+            image.image = UIImage(named: "solidGray")
+        }
     }
 
+    @IBAction func regularDownload(sender: AnyObject) {
+    }
 
+    @IBAction func serialDownload(sender: AnyObject) {
+    }
+    
+    @IBAction func concurrentDownload(sender: AnyObject) {
+    }
 }
 
